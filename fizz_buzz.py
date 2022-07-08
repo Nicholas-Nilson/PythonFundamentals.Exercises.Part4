ab3803daff1f62964fcd246231b92a31cf1406ba
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-08 16:31:00 -0400

    random update

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,16 +1,18 @@
 input_range = range(1,101)
 
+
 def fizz_buzz(range_to_check):
-    '''Fizz & Buzz; partners in crime, multiples of 15 are in shambles'''
+    ''''Fizz & Buzz; partners in crime, multiples of 15 are in shambles'''
     fizz_buzz_range = range_to_check
     for num in fizz_buzz_range:
-        if (num % 15 ==0):
+        if num % 15 ==0:
             print("FizzBuzz")
-        elif (num % 3 == 0):
+        elif num % 3 == 0:
             print("Fizz")
-        elif (num % 5 == 0):
+        elif num % 5 == 0:
             print("Buzz")
         else:
             print(num)
+
 
 fizz_buzz(input_range)
